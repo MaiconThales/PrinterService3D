101913c75b8f87ae5700a74cd0ae42ce70e53d54
Maicon Thales <maiconthales34@hotmail.com>
2023-09-27 16:28:08 -0300

    Add roles.

--- src/main/java/com/printer/controller/ExampleController.java
+++ src/main/java/com/printer/controller/ExampleController.java
@@ -1,5 +1,6 @@
 package com.printer.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,21 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ExampleController {
 
+	@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_MODERATOR', 'ROLE_ADMIN')")
 	@GetMapping("/mensagem")
 	public String obterMensagem() {
 		return "Olá, este é um exemplo de serviço Web!";
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping("/enviar")
 	public String enviarMensagem(@RequestBody String mensagem) {
 		return "Mensagem recebida: " + mensagem;
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PutMapping("/atualizar/{id}")
 	public String atualizarMensagem(@PathVariable Long id, @RequestBody String novaMensagem) {
 		return "Mensagem com ID " + id + " atualizada com: " + novaMensagem;
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@DeleteMapping("/excluir/{id}")
 	public String excluirMensagem(@PathVariable Long id) {
 		return "Mensagem com ID " + id + " excluída com sucesso.";
